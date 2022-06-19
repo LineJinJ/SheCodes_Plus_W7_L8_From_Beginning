7fdc3ab91e35d991556965ec195ea841cc5ed6b4
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-19 13:22:48 +0200

    Added JS to function handleSubmit

--- src/app.js
+++ src/app.js
@@ -48,3 +48,9 @@ function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
