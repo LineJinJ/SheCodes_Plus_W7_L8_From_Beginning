574492aca16bdee8d2b94acc49a3dd19bf911370
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-19 13:35:03 +0200

    Added JS to celsiusLink

--- src/app.js
+++ src/app.js
@@ -79,3 +79,6 @@ form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
